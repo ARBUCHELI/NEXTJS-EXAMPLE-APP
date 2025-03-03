1c87b9fd9a043b71eed84c616da81bfa6c32fda7
ARBUCHELI <radio_seattle@hotmail.com>
2025-03-03 15:23:34 +0300

    the useRouter() hook

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,14 +1,14 @@
 'use client'
 import styles from './Footer.module.css'
-
+import { useRouter } from 'next/navigation'
 export default function Footer() {
-  const router = "REPLACE ME"
-
+  const router = useRouter();
+  const callingPush = () => router.push("/about")
   return (
     <footer className={styles.footer}>
-      <button>Back</button>
-      <button>Home</button>
-      <button>Forward</button>
+      <button onClick={router.back}>Back</button>
+      <button onClick={callingPush}>Home</button>
+      <button onClick={router.forward}>Forward</button>
     </footer>
   )
 }
