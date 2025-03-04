5111fc18c56d3ccd6d9c9eb536c87d944e6b71ba
ARBUCHELI <radio_seattle@hotmail.com>
2025-03-05 01:04:33 +0300

    editing about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,13 @@
 export default function AboutPage() {
     return (
-      <p>
-        This application contains snippets from various Codecademy articles. You can browse all our articles and read them in their entirety <a href="https://www.codecademy.com/articles">here</a>.
-      </p>
+      <>
+        <p>
+          This application contains snippets from various Computer Science and Programming articles that appear in the prestigious educational platform Codecademy. 
+        </p>
+        <p>
+          You can browse all our articles and read them in their entirety <a href="https://www.codecademy.com/articles">here</a>.
+        </p>
+      </>
+    
     )
   }
